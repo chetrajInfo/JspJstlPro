2f27106ac767f1eaa8f008b0ef597f5803c22c71
myinfo <chetraj0101@gmail.com>
2023-08-11 01:10:36 -0500

    changes in index page and controller class to display h1 tag

--- src/main/java/com/example/springjspdemo/controller/HomeController.java
+++ src/main/java/com/example/springjspdemo/controller/HomeController.java
@@ -1,15 +1,28 @@
 package com.example.springjspdemo.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    public String home(Model model) {
+    public String index(Model model) {
+        // Set attribute for conditionally displaying the <h1> tag
+        model.addAttribute("someCondition", true);
+
+        // Set attributes for dynamic values for color and title
+        model.addAttribute("colorValue", "green");
+        model.addAttribute("pageTitle", "Dynamic Page Title");
+
+        // Set attribute for user role
+        // This can be fetched from a logged-in user's session or some other logic.
+        // For simplicity, I'm hardcoding it here.
+        model.addAttribute("role", "admin");
+
+        // Set attribute for displaying Hello message
         model.addAttribute("name", "World");
-        return "index";
+
+        return "index"; // This refers to the index.jsp view
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,7 +5,29 @@
     <title>Spring JSP Demo</title>
 </head>
 <body>
-<h1 style="color:blue; text-align:center;">Welcome to the JSP and JSTL Page Demo.</h1>
-    Hello, <c:out value="${name}" />!
+
+<!-- Conditionally displaying the <h1> tag -->
+<c:if test="${someCondition}">
+    <h1 style="color:blue; text-align:center;">Welcome to the JSP and JSTL Page Demo.</h1>
+</c:if>
+
+<!-- Dynamic values for color and title -->
+<h1 style="color:${colorValue}; text-align:center;">Welcome to the ${pageTitle}.</h1>
+
+<!-- Multiple conditions for displaying title based on user roles -->
+<c:choose>
+    <c:when test="${role == 'admin'}">
+        <h1 style="color:red; text-align:center;">Admin Dashboard</h1>
+    </c:when>
+    <c:when test="${role == 'user'}">
+        <h1 style="color:blue; text-align:center;">User Homepage</h1>
+    </c:when>
+    <c:otherwise>
+        <h1 style="color:gray; text-align:center;">Welcome!</h1>
+    </c:otherwise>
+</c:choose>
+
+Hello, <c:out value="${name}" />!
+
 </body>
 </html>
